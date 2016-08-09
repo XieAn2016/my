5e2bf69245c4a2aa9fe30d395139a927394f0d5c
XieJing2016 <xjysyy@gmail.com>
2016-08-09 23:24:03 +0800

    完善ing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 /**
  * Created by lenovo on 2016/8/8.
  */
+;$(function () {
+    var sidebar=$('#sidebar'),
+    	mask=$('.mask'),
+		sidebar_trigger=$('#sidebar_trigger');
+
+	function showSidebar() {
+		mask.fadeIn();
+		sidebar.css('right',0);
+	}
+
+	function hideSidebar(){
+		mask.fadeOut();
+		sidebar.css('right',-sidebar.width());
+	}
+
+    sidebar_trigger.on('click',showSidebar);
+    mask.on('click',hideSidebar);
+});
