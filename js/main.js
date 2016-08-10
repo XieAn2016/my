16db698dc4428202f896572ac6d1dcd6a8514f65
XieJing2016 <xjysyy@gmail.com>
2016-08-10 23:40:39 +0800

    返回顶部的完善

--- js/main.js
+++ js/main.js
@@ -18,4 +18,21 @@
 
     sidebar_trigger.on('click',showSidebar);
     mask.on('click',hideSidebar);
+
+   	var backButton=$('.back-to-top');
+   	backButton.on('click',function(){
+   		$('html,body').animate({
+   			scrollTop:0
+   		},800);
+   	})
+
+   	$(window).on('scroll',function(){
+   		if ($(window).scrollTop()>$(window).height()) {
+   			backButton.fadeIn();
+   		}else{
+   			backButton.fadeOut();
+   		}
+   	})
+
+   	$(window).trigger('scroll');
 });
